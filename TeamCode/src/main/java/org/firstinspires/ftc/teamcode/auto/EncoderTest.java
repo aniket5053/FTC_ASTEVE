9edec16efaa6b204cc8f3d6b1aa137126884b4c9
Aniker Sonika <as7434@rit.edu>
2023-12-07 21:38:41 -0500

    test time based auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/EncoderTest.java
@@ -4,39 +4,38 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "EncoderTest (Blocks to Java)")
+@Autonomous(name = "EncoderTest")
 public class EncoderTest extends LinearOpMode {
-
-    private DcMotor LEFTDRIVE;
-    private DcMotor RIGHTDRIVE;
 
     /**
      * This function is executed when this OpMode is selected from the Driver Station.
      */
     @Override
     public void runOpMode() {
-        double StartTimes;
 
-        LEFTDRIVE = hardwareMap.get(DcMotor.class, "LEFT DRIVE");
-        RIGHTDRIVE = hardwareMap.get(DcMotor.class, "RIGHT DRIVE");
 
-        // Put initialization blocks here.
-        LEFTDRIVE.setDirection(DcMotor.Direction.REVERSE);
-        LEFTDRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RIGHTDRIVE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LEFTDRIVE.setTargetPosition(100);
-        RIGHTDRIVE.setTargetPosition(100);
-        LEFTDRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RIGHTDRIVE.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        DcMotor LEFTDRIVE = hardwareMap.get(DcMotor.class, "LEFT DRIVE");
+        DcMotor RIGHTDRIVE = hardwareMap.get(DcMotor.class, "RIGHT DRIVE");
+
         waitForStart();
-        LEFTDRIVE.setPower(0.8);
-        RIGHTDRIVE.setPower(0.8);
-        StartTimes = getRuntime();
-        while (opModeIsActive() && getRuntime() - StartTimes < 1.5) {
-            // Put loop blocks here.
-            telemetry.update();
-        }
+
+        LEFTDRIVE.setPower(1);
+        RIGHTDRIVE.setPower(1);
+
+        sleep(1000);
+
+        LEFTDRIVE.setPower(-0.5);
+        RIGHTDRIVE.setPower(0.5);
+        sleep(750);
+
+        LEFTDRIVE.setPower(0.5);
+        RIGHTDRIVE.setPower(-0.5);
+        sleep(750);
+
         LEFTDRIVE.setPower(0);
         RIGHTDRIVE.setPower(0);
+        sleep(1000);
+
+
     }
 }
